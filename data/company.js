1c00374e27bf73df89134e52a85106530e973d70
omkarakhade <77960829+omkarakhade@users.noreply.github.com>
2026-07-02 19:35:17 +0530

    Add company information and office details

---
+++ data/company.js
@@ -0,0 +1,65 @@
+export const company = {
+  name: "Delta Engineering Works",
+  tagline: "Solutions for Welding & Cutting",
+  founded: "An ISO 9001:2000 Company",
+  emails: [
+    "sales@deltawelds.com",
+    "deltaweng29@rediffmail.com",
+    "deltaeltahkam@gamil.com",
+  ],
+  websites: ["www.deltawelds.com", "www.deltaengineeringworks.com"],
+  presenceNote:
+    "We have offices in Saudi Arabia, Bahrain, Oman, UAE, Kuwait, and Qatar.",
+  offices: [
+    {
+      name: "Factory Address (India)",
+      lines: [
+        "Delta Engineering Works,",
+        "C/A-1/1, Block MIDC, Behind IBMR College,",
+        "Chinchwad, Pune - 411019, India",
+      ],
+      phones: ["+91 98221 90996", "+91 93733 26314"],
+      primary: true,
+    },
+    {
+      name: "Muscat Office",
+      lines: [
+        "Establishment Delta El-Tahkam for Projects SPC,",
+        "Shop No W, Al Raha 2 Building, Block 1,",
+        "Al Khoudh, Al Seeb, Sultanate of Oman",
+      ],
+      phones: ["+966 50 230 2236", "+91 93254 08403", "+973 3513 0496", "+968 9447 1135"],
+    },
+    {
+      name: "Sohar Office",
+      lines: [
+        "Flat No 7, Third Floor, Above Asas Suwaihra Building,",
+        "Building No. 131, Falaj Al Qabail,",
+        "Sohar, Sultanate of Oman",
+      ],
+      phones: ["+968 9535 8395"],
+    },
+    {
+      name: "Bahrain Office",
+      lines: ["1504, Seef Terraces,", "Al-Seef District, Bahrain"],
+      phones: ["+973 3513 0496"],
+    },
+    {
+      name: "Saudi Arabia Office",
+      lines: [
+        "Delta Control Projects Est.,",
+        "Building No. 5148, King Khalid Road,",
+        "8173 AlRabie District, Postal Code 32241, Dammam, Saudi Arabia",
+      ],
+      phones: ["+966 50 230 2236"],
+    },
+  ],
+  certifications: [
+    "ISO 9001:2000 Certified Company",
+    "SASO Certified",
+    "Aramco Approved Vendor Network",
+    "SABIC Approved Vendor Network",
+    "Member of Multilateral Recognition Arrangement (IAF)",
+    "Indian Achievers' Forum",
+  ],
+};
