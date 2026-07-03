af084f43c208c5c39779c95f36558b17dff5c502
omkarakhade <77960829+omkarakhade@users.noreply.github.com>
2026-07-03 16:09:36 +0530

    Add logoDataUri for SVG logo

---
+++ data/logo.js
@@ -0,0 +1,2 @@
+export const logoDataUri =
+  "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%20500%20436%22%3E%3Cpolygon%20points%3D%22250%2C0%200%2C436%20500%2C436%22%20fill%3D%22%230A1B4E%22/%3E%3C/svg%3E";
